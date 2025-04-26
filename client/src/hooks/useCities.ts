6313d4e17763fdf828a6371ba31902d6b3d11f0c
Shahar <shahar.g@myzorro.co>
2025-04-26 18:23:30 +0300

    fix: json url work on prod

--- client/src/hooks/useCities.ts
+++ client/src/hooks/useCities.ts
@@ -23,7 +23,7 @@ export const useCities = () => {
     async function fetchJson() {
       try {
         setLoading(true);
-        const response = await fetch(filePath);
+        const response = await fetch(import.meta.env.BASE_URL + filePath);
         if (!response.ok) {
           throw new Error(`Failed to fetch JSON: ${response.statusText}`);
         }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tax-heaven-il",
+  base: "/tax-heaven-il/",
   plugins: [react()],
-})
+});
